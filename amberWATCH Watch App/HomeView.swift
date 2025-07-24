c2b1cfc1ccea946b53eab5141959ae4a6e0cea7f
immanuel <89377408+username11384@users.noreply.github.com>
2025-07-24 21:08:58 +1000

    Update HomeView.swift
    snap back to top

--- amberWATCH Watch App/HomeView.swift
+++ amberWATCH Watch App/HomeView.swift
@@ -6,56 +6,65 @@ struct HomeView: View {
     @Binding var currentTab: TabSelection
 
     var body: some View {
-        VStack(spacing: 0) { // Use spacing: 0 to remove default spacing between elements
-            Rectangle()
-                .fill(Color.black)
-                .frame(height: 0.3)
+        ScrollViewReader {
+            proxy in
+            VStack(spacing: 0) { // Use spacing: 0 to remove default spacing between elements
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(height: 0.3)
 
-            ScrollView {
-                VStack {
-                    Text("Amber Prices")
-                        .font(.headline)
-                        .padding(.bottom, 20)
-
-                    VStack(alignment: .leading) {
-                        Text("Import Price:")
+                ScrollView {
+                    VStack {
+                        Text("Amber Prices")
                             .font(.headline)
-                        Text(dataStore.liveImportPrice)
-                            .font(.title2)
-                            .foregroundColor(PriceColorHelper.importPriceColor(for: Double(dataStore.liveImportPrice.replacingOccurrences(of: "¢", with: "")) ?? 0))
+                            .padding(.bottom, 20)
+
+                        VStack(alignment: .leading) {
+                            Text("Import Price:")
+                                .font(.headline)
+                            Text(dataStore.liveImportPrice)
+                                .font(.title2)
+                                .foregroundColor(PriceColorHelper.importPriceColor(for: Double(dataStore.liveImportPrice.replacingOccurrences(of: "¢", with: "")) ?? 0))
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                        .onTapGesture {
+                            currentTab = .importTab
+                        }
+
+                        VStack(alignment: .leading) {
+                            Text("FiT Price:")
+                                .font(.headline)
+                            Text(dataStore.liveExportPrice)
+                                .font(.title2)
+                                .foregroundColor(PriceColorHelper.exportPriceColor(for: Double(dataStore.liveExportPrice.replacingOccurrences(of: "¢", with: "")) ?? 0))
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                        .onTapGesture {
+                            currentTab = .exportTab
+                        }
+
+                        Button(action: {
+                            dataStore.fetchData()
+                        }) {
+                            Label("Refresh Data", systemImage: "arrow.clockwise.circle.fill")
+                                .font(.headline)
+                        }
+                        .padding(.top)
                     }
                     .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(10)
-                    .onTapGesture {
-                        currentTab = .importTab
-                    }
-
-                    VStack(alignment: .leading) {
-                        Text("FiT Price:")
-                            .font(.headline)
-                        Text(dataStore.liveExportPrice)
-                            .font(.title2)
-                            .foregroundColor(PriceColorHelper.exportPriceColor(for: Double(dataStore.liveExportPrice.replacingOccurrences(of: "¢", with: "")) ?? 0))
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(10)
-                    .onTapGesture {
-                        currentTab = .exportTab
-                    }
-
-                    Button(action: {
-                        dataStore.fetchData()
-                    }) {
-                        Label("Refresh Data", systemImage: "arrow.clockwise.circle.fill")
-                            .font(.headline)
-                    }
-                    .padding(.top)
+                    .id("topOfHomeView") // Identifier for scrolling
                 }
-                .padding()
+                .onChange(of: currentTab) { newTab in
+                    if newTab == .home {
+                        withAnimation { proxy.scrollTo("topOfHomeView", anchor: .top) }
+                    }
+                }
             }
         }
         .onAppear {
